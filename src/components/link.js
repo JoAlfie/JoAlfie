3cea7a0be089f69345220b1387c03a4b14e8ca9a
Jo Alfie Wimborne <jowimborne@gmail.com>
2019-09-22 11:40:30 +0100

    Add rel tag for security

--- src/components/link.js
+++ src/components/link.js
@@ -26,7 +26,7 @@ const Link = ({ children, to, activeClassName, partiallyActive, ...other }) => {
 		)
 	}
 	return (
-		<a href={to} {...other}>
+		<a href={to} {...other} rel="noopener noreferrer">
 			{children}
 		</a>
 	)
