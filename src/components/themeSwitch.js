0c69ba97b717a347f844604a626265db2768a7d4
Jo Alfie Wimborne <jowimborne@gmail.com>
2019-09-22 01:44:19 +0100

    Use fontawesome icons for theme switch consistency

--- src/components/themeSwitch.js
+++ src/components/themeSwitch.js
@@ -1,5 +1,6 @@
 import React from "react"
 import useDarkMode from "use-dark-mode"
+import { FaMoon, FaSun } from "react-icons/fa"
 import "../styles/themeswitch.less"
 
 const ThemeSwitch = () => {
@@ -16,14 +17,14 @@ const ThemeSwitch = () => {
 				onClick={handleLightPress}
 				aria-label="Switch to light mode"
 			>
-				☀
+				<FaSun />
 			</button>
 			<button
 				className="themeswitch__button themeswitch__button--dark"
 				onClick={handleDarkPress}
 				aria-label="Switch to dark mode"
 			>
-				☾
+				<FaMoon />
 			</button>
 		</div>
 	)
